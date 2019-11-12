50dfbfd3168f3befc28b7dc92ce5e23bb969d2d3
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-12 14:08:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ function setThisWithApply(fn, thisValue, args) {
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue) {
-  const copy = functionToBeCopied
+  const copy = functionToBeCopied.bind(thisValue);
+  
 }
